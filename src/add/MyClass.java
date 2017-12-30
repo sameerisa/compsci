29361d9a945024feff63aedb7a0adf7ba2b9adb2
sameerisa <sameersamisa@gmail.com>
2017-12-30 03:55:16 -0500

    add proj final

--- src/add/MyClass.java
+++ src/add/MyClass.java
@@ -13,19 +13,22 @@ public class MyClass {
 		Student[] s	= new Student[20];
 		int size = 0;
 		boolean running = true;
-		Scanner read = new Scanner(System.in);
+		
 	
 		while(running) 
 		{
+			Scanner read = new Scanner(System.in);
 			System.out.println("What do you want to do");
 			String temp = read.nextLine();
 			
 			if(temp.equals("add"))
 			{
+				
 				System.out.println("Enter name followed by 3 test scores: ");
 				Student s1 = new Student(read.nextLine(),read.nextInt(),read.nextInt(),read.nextInt());
-				
-				int index = laste(s);
+				read.nextLine();
+				addElement(s1, s, size);
+				/*int index = laste(s);
 				
 				if(index == -1) 
 				{
@@ -38,28 +41,51 @@ public class MyClass {
 					
 				}
 				
-				
-				
+				if(index==-1) 
+					size=19;
+				else
+					size = laste(s)-1;
+				printArrayAvg(s,size);*/
 			}
 			if(temp.equals("delete")) 
 			{
-				int index = laste(s);
+				deleteElement( s, size);
+				/*int index = laste(s);
+				if(index!=0) {
 				if(index==-1) 
 				{
 					s[19]=null;
 				}
 				else
 					s[(index-1)]=null;
+				}
+				else {
+			if(index==-1) 
+				size=19;
+			else
+				size = laste(s)-1;
+			printArrayAvg(s,size);
 			}
-			size = laste(s)-1;
+			if(laste(s)==-1) 
+			{
+				size = 19;
+			}
+			else
+				size = laste(s)-1;
+				
+				*/
+			}
 			if(temp.equals("quit")) 
 			{
 				running = false;
 			}
-		
+			
+			
 		}
 		printArray(s,size);
 	}
+		
+	
 	
 	private static int laste(Student[] s) 
 	{
@@ -68,6 +94,7 @@ public class MyClass {
 			if(s[i]==null) 
 			{
 				index = i;
+				break;
 			}
 			
 		}
@@ -80,6 +107,27 @@ public class MyClass {
 		}
 	}
 	private static boolean addElement(Student s1, Student[] s, int size) {
+		
+		
+		int index = laste(s);
+		
+		if(index == -1) 
+		{
+			System.out.println("The array is full");
+			
+			
+		}
+		else 
+		{
+			s[index]=s1;
+			
+		}
+		
+		if(index==-1) 
+			size=19;
+		else
+			size = laste(s)-1;
+		printArrayAvg(s,size);
 		if(laste(s)!=-1){
 		if(s1.getName()==s[size].getName()) {
 			return true;
@@ -95,22 +143,67 @@ public class MyClass {
 		}
 		
 	}
-	private static boolean deleteElement(Student s1, Student[] s, int size) {
-		if(s[size+1]==null) {
+	private static boolean deleteElement(Student[] s, int size) {
+		int index = laste(s);
+		if(index!=0) {
+			
+		if(index==-1) 
+		{
+			s[19]=null;
+			
+		}
+		else
+			s[(index-1)]=null;
+			
+		
+		}
+		else {
+			System.out.println("Array is empty");
+			
+	
+
+		}
+	
+		if(laste(s)==0) {
+			size=0;
+		}
+		else
+		{
+			size=laste(s)-1;
+		}
+		printArrayAvg(s,size);
+	
+		if(s[size]==null) {
 			return true;
 		}
 		else 
 		{
-			false;
+			return false;
 		}
+		
+		
 	}
 	private static void printArray(Student[] s, int size) {
-		for(Student i:s) 
+		if(size!=0) {
+		for(int i=0; i<=size;i++) 
 		{
-			System.out.print(i+" ");
+			System.out.print(s[i].getName()+" ");
 			
 		}
 		System.out.println();
 	}
+	}
+	private static void printArrayAvg(Student[] s, int size) {
+		if(laste(s)!=0) {
+		for(int i=0; i<=size;i++) 
+		{
+			System.out.print(s[i].getName()+" "+s[i].getAverage()+" ");
+			
+		}
+		System.out.println();
+	}
+		else {}
+	}
+
 
 }
