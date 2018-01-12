21a14c0b836891bfb40f15bf379d52cc1da613aa
samee <samee@172.20.105.166>
2018-01-12 10:42:46 -0500

    finish search

--- src/searching/Search.java
+++ src/searching/Search.java
@@ -11,13 +11,14 @@ public class Search {
 		System.out.println("Enter in 10 numbers in increasing order");
 		int[] num = new int[10];
 		num[0]=read.nextInt();
-		while(n>=9) 
+		
+		while(true) 
 		{
 			nnum=read.nextInt();
 			
-			if(nnum>num[n-1])
+			if(nnum>=num[n-1])
 			{
-			num[n]=read.nextInt();
+			num[n]=nnum;
 			
 			
 			}
@@ -27,9 +28,9 @@ public class Search {
 				{
 					System.out.println("Invalid enter another number");
 					nnum=read.nextInt();
-					if(nnum>num[n-1]) 
+					if(nnum>=num[n-1]) 
 					{
-						num[n]=read.nextInt();
+						num[n]=nnum;
 						break;
 					}
 				}
@@ -41,6 +42,31 @@ public class Search {
 			n++;
 			
 			
+		}
+		for(int i:num) 
+		{
+			System.out.print(i+" ");
+			
+		}
+		System.out.println();
+		System.out.println("What number do you want to search for");
+		nnum=read.nextInt();
+		int index=-1;
+		for(int i = 0; i<num.length; i++) 
+		{
+			if(num[i]==nnum) 
+			{
+				index=i;
+				break;
+			}
+		}
+		if(index==-1) 
+		{
+			System.out.println("Does not exist");
+		}
+		else 
+		{
+			System.out.println("The number appears at "+index);
 		}
 	}
 
