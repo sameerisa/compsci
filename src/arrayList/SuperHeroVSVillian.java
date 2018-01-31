201635bfd7b7521cc972de979ab445df8f2dbf1f
samee <samee@172.20.110.175>
2018-01-31 10:40:03 -0500

    done?

--- src/arrayList/SuperHeroVSVillian.java
+++ src/arrayList/SuperHeroVSVillian.java
@@ -1,5 +1,6 @@
 package arrayList;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
@@ -12,55 +13,51 @@ public class SuperHeroVSVillian {
 		
 		
 		Scanner read = new Scanner(System.in);
+		ArrayList<Hero> hero= new ArrayList<Hero>();
+		ArrayList<Villain> vil= new ArrayList<Villain>();
 		
 		p("Welcome to your customized superhero story");
-		p("Enter your heros alias, superpower and then the amount of health your hero will have");
-		Hero hero = new Hero(read.nextLine(),read.nextLine(),read.nextInt());
-		read.nextLine();
-		p("Enter your villains alias, evil power and then the amount of health");
-		Villain villain = new Villain(read.nextLine(),read.nextLine(),read.nextInt());
-		p(hero.toString());
-		p(villain.toString());
-		
-		p("The story begins in the city.");
-		delay(2);
-		psc(villain.getalias()+" killed "+hero.getalias()+"'s father. \n");
-		delay(2);
-		psc("Ever since then "+hero.getalias()+" has been fighting crime to supress his hatred towards "+villain.getalias()+".\n");
-		delay(2);
-		psc("One day "+villain.getalias()+" was loitering at the park.\n");
-		delay(1);
-		psc("All the parents were in outrage. \n");
-		delay(2);
-		psc("Suddenly in the sky "+hero.getalias()+" was flying by. \n");
-		delay(1);
-		psc("The parents stood in awe. \n");
-		delay(2);
-		psc("As "+hero.getalias()+" flew down "+villain.getalias()+" used his "+villain.getevilpower()+" on "+hero.getalias() +"\n");
-		delay(2);
-		psc("It wasent very effective. \n");
-		delay(1);
-		psc("A battle broke out. \n");
-		delay(1);
-		psc(hero.getalias()+" used "+hero.getsuperpower()+"\n");
-		delay(1);
-		fight(hero, villain);
-		delay(1);
-		while(hero.gethealth()!=0 && villain.gethealth()!=0)
-			fight(hero, villain);
-		
-		if(hero.gethealth()==0) {
-			psc(hero.getalias()+" was defeated. "+villain.numberofPeopleHarmed()+" people were hurt." + "\n");
-			psc("To this day "+villain.getalias()+" continues to reign terror over the city");
+		String ans = null;
+		while(true)
+		{
+			p("Enter your heros alias, superpower and then the amount of health your hero will have or enter quit to continue");
+			ans = read.nextLine();
+			if(ans.equals("quit"))
+				break;
+			hero.add(new Hero(ans,read.nextLine(),read.nextInt()));
+			read.nextLine();
 		}
-		if(villain.gethealth()==0) {
-			psc(villain.getalias()+" was defeated. "+hero.numberofPeopleSaved()+ " people were saved \n");
+		
+		
+		while(true)
+		{
+			p("Enter your villains alias, evil power and then the amount of health");
+			ans = read.nextLine();
+			if(ans.equals("quit"))
+				break;
+			vil.add(new Villain(ans,read.nextLine(),read.nextInt()));
 			
-			psc("To this day "+hero.getalias()+" continues to clean the streets from crime");
 		}
+		Villain villain = new Villain(read.nextLine(),read.nextLine(),read.nextInt());
+		System.out.println(hero);
+		System.out.println(vil);
+		while(hero.isEmpty() && vil.isEmpty());
+		{
+			
+			int h = (int)Math.random()*hero.size()-1;
+			int v = (int)Math.random()*vil.size()-1;
+		
+			while(vil.get(v).gethealth()!=0 && hero.get(h).gethealth() !=0)
+			{
+				psc(vil.get(v)+" attacks "+hero.get(h));
+				
+			}
 		
 		
 		
+		
+		
+		}
 		
 	}
 	
