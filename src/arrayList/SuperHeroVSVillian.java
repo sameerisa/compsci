e2868e83268898980abd890c9aaba326cf2fbaba
samee <samee@10.0.0.170>
2018-02-01 23:46:57 -0500

    done

--- src/arrayList/SuperHeroVSVillian.java
+++ src/arrayList/SuperHeroVSVillian.java
@@ -36,38 +36,51 @@ public class SuperHeroVSVillian {
 			if(ans.equals("quit"))
 				break;
 			vil.add(new Villain(ans,read.nextLine(),read.nextInt()));
+			read.nextLine();
 			
 		}
-		read.nextLine();
+	
 		System.out.println(hero);
 		System.out.println(vil);
-		while(hero.isEmpty() && vil.isEmpty());
+		
+		while(hero.size()!=0 && vil.size()!=0)
 		{
 			
-			int h = (int)Math.random()*hero.size()-1;
-			int v = (int)Math.random()*vil.size()-1;
+			int h = (int)(Math.random()*(hero.size()-1));
+			int v = (int)(Math.random()*(vil.size()-1));
 		
 			while(vil.get(v).gethealth()!=0 && hero.get(h).gethealth() !=0)
 			{
-				psc(vil.get(v)+" attacks "+hero.get(h));
+				
 				fight(hero.get(h), vil.get(v));
 				
 				
 			}
 			if(vil.get(v).gethealth()==0) 
 			{
+				
 				psc(vil.get(v).getalias()+" has been defeated");
+				System.out.println();
+				vil.remove(v);
 			}
 			if(hero.get(h).gethealth()==0)
+			{
 				psc(hero.get(h).getalias()+" has been defeated");
+				System.out.println();
+				hero.remove(h);
+			}
+			
 		
 		
 		
 		
 		}
-		if(hero.isEmpty())
+		System.out.println();
+		
+		
+		if(hero.size()==0)
 			psc("The hero's have been defeated");
-		if(vil.isEmpty()) 
+		if(vil.size()==0) 
 			psc("The villain's have been defeated");
 		
 	}
